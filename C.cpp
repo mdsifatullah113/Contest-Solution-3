7793c037187c1f4fd93bdd61865ea7ce6c61a99e
Sifatullah <s2410476116@ru.ac.bd>
2026-04-08 19:58:25 +0600

    C solution

---
+++ C.cpp
@@ -0,0 +1,184 @@
+/* ---------------------------------------------------In the name of ALLAH --------------------------------------------------*/
+/* ------------------------------------------LA ILAHA ILLALLAH MUHAMMAD RASHULALLAH------------------------------------------*/
+/* ---------------------------------------------------- ALLAHU AKBAR --------------------------------------------------------*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/* ---------- Typedefs ---------- */
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<ll> vl;
+typedef set<int> si;
+typedef set<ll> sl;
+typedef vector<vi> vvi;
+typedef vector<vc> vvc;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+typedef float fl;
+
+/* ---------- Macros ---------- */
+#define PB push_back
+#define F first
+#define S second
+#define MP make_pair
+#define endl '\n'
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define sz(x) (int)(x).size()
+#define mid(l, r) ((l + r) / 2)
+#define left(node) ((node) * 2)
+#define right(node) ((node) * 2 + 1)
+#define mem(a, b) memset(a, b, sizeof(a))
+#define gcd(a, b) __gcd(a, b)
+#define sqr(a) ((a) * (a))
+
+/* ---------- Constants ---------- */
+const double PI = 3.141592653;
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000LL;
+const ll MOD = 1000000007;
+const int mx_int_prime = 999999937;
+
+/* ---------- Fast IO ---------- */
+#define optimize()               \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL);               \
+    cout.tie(NULL);
+#define fraction()                \
+    cout.unsetf(ios::floatfield); \
+    cout.precision(10);           \
+    cout.setf(ios::fixed, ios::floatfield);
+#define file()                        \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+
+/* ---------- Iterators ---------- */
+typedef vector<int>::iterator vit;
+typedef set<int>::iterator sit;
+
+/* ---------- Direction Arrays ---------- */
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+// int dx[] = {1, 0, -1, 0, 1, 1, -1, -1};
+// int dy[] = {0, 1, 0, -1, 1, -1, 1, -1};
+
+// const int mx = 1e5;
+vl v;
+/*For Division 1: 1900≤rating
+For Division 2: 1600≤rating≤1899
+For Division 3: 1400≤rating≤1599
+For Division 4: rating≤1399
+*/
+ll solve1(ll n, ll c1, ll m)
+{
+
+    set<ll> s;
+    vl v1;
+
+    for (ll i = 0; i < n; i++)
+    {
+        s.insert(v[i]);
+    }
+    for (auto u : s)
+    {
+        v1.push_back(u);
+    }
+    ll c = v1.size();
+    ll mx = *max_element(all(v1));
+    for (ll i = 0; i < mx; i++)
+    {
+        if (v1[i] != i)
+        {
+            c = i;
+            break;
+        }
+    }
+    if (c == m)
+    {
+        return 0;
+    }
+    ll cnt = 0;
+
+    for (int i = 0; i < m; i++)
+    {
+        if (s.find(i) == s.end())
+        {
+            cnt++;
+        }
+    }
+    ll l1 = max(cnt, c1);
+    // if (cnt > 0 && c1 > 0)
+    // {
+    //     // if (cnt > c1)
+    //     // {
+    //     //     l1 -= c1;
+    //     // }
+    //     l1--;
+    // }
+    return l1;
+}
+
+void print(ll n)
+{
+    // if (n)
+    // {
+    //     cout << "YES";
+    // }
+    // else
+    // {
+    //     cout << "NO";
+    // }
+
+    cout << n;
+    cout << endl;
+}
+
+int main()
+{
+    optimize();
+    int t;
+    cin >> t;
+    while (t--)
+
+    {
+
+        ll m, n, a, b, k, c, c1 = 0, c2 = 0;
+
+        cin >> n >> m;
+        // string s1, s2, s3;
+        // cin >> s1 >> s2 >> s3;
+        // v.assign(n, vc(m));
+        v.resize(n);
+        for (auto &u : v)
+        {
+            cin >> u;
+            if (u == m)
+            {
+                c1++;
+            }
+        }
+        // cin >> a;
+        // for (ll i = 0; i < n; i++)
+        // {
+        //     for (ll j = 0; j < m; j++)
+        //     {
+        //         cin >> v[i][j];
+        //     }
+        // }
+        // sort(all(v));
+
+        print(solve1(n, c1, m));
+        // cout << endl;
+        v.clear();
+    }
+
+    return 0;
+}
